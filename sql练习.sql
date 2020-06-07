30176fa1f44131c49bc4e9e434c9852e1a5b3eae
Leo ZIYUAN <58551355+ZiyuanLan@users.noreply.github.com>
2020-06-07 19:25:53 +0800

    Update sql练习.sql

--- sql练习.sql
+++ sql练习.sql
@@ -23,207 +23,108 @@ SELECT product_id, amount FROM sales
 where amount between 100 and 500;
 
 #从销售表中，找出销售数量在5件以上同时配送城市为武汉的会员ID#
-
 SELECT member_id from sales 
-
 WHERE quant >5 AND delivery_city = "wuhan";
 
-
-
 #销售表中筛选出武汉和上海的会员ID# (范围匹配)
-
 select member_id from sales 
-
 where city in ("wuhan","shanghai");
 
-
-
 #从会员表中筛选出手机号码最后含09以及姓张的会员ID、手机号码和姓名#
-
 SELECT member_id, phone, name FROM members 
-
 WHERE phone LIKE '%09' AND name LIKE '张%'
 
-
-
 #请从销售表中，找出南京白下区交易金额大于1000的门店名称以及交易金额# （模糊匹配）
-
 SELECT shop_name, amount FROM sales 
-
 WHERE shop_name LIKE "南京白下区%" AND amount > 1000;
 
-
-
 #将销售表中的交易金额/交易量定义为单价#
-
 SELECT amount/quant AS price FROM sales;
-
-
 
 ------------------------------------------------------------
 
-
-
 #第三章 表连接#
-
 
 
 (INNER场景：找到会员的消费金额)
 
 #使用会员ID链接销售表与会员表，找出购买产品的会员ID以及消费金额#
-
 SELECT m.member_id, s.amount
-
 FROM sales s
-
 INNER JOIN members m
-
 ON s.member_id=m.member_id;
 
 
 
 (RIGHT场景：找到会员与非会员的交易金额)
-
 #使用会员表中的member_id匹配销售表中的member_id，查询购买产品的会员ID以及购买金额。（如果为非会员，则member_id为空值null）#
-
 SELECT m.member_id, s.amount
-
 FROM members m
-
 RIGHT JOIN sales s
-
 ON m.member_id=s.member_id;
 
 
-
-(LEFT场景：找到未购买产品的会员)
-
+(LEFT：找到未购买产品的会员)
 #找到未购买产品的会员，他的名字，年龄，性别#
-
 SELECT name, age, gender
-
 FROM members m
-
 LEFT JOIN sales s
-
 on m.member_id = s.member_id
-
 WHERE amount is NULL;
-
-
 
 ---------------------------------------------------------
 
-
-
 #第四章 数据分组和排序#
 
-
-
 #从销售表中找到业绩最好的门店#
-
 SELECT shop_name, SUM(amount) AS total
-
 FROM sales
-
 Group BY shop_name
-
 ORDER BY total;
 
-
-
 #将会员的性别改为中文#
-
 SELECT
-
 CASE gender
-
 WHEN 'm'THEN'男'
-
 WHEN 'w'THEN'女'
-
 END AS flag
-
 FROM members;
 
-
-
 #找出销售金额最高的产品大类与产品小类# (HAVING / WHERE 用法)
-
 SELECT base_category, category, SUM(amount) AS total
-
 FROM sales s
-
 INNER JOIN product p
-
 ON s.product_id=p.product_id
-
 GROUP BY base_category, category
-
 HAVING total > 250000
-
 ORDER BY total DESC;
 
-
-
 SELECT base_category, category, SUM(amount) AS total
-
 FROM sales s
-
 INNER JOIN product p
-
 ON s.product_id=p.product_id
-
 WHERE amount >= 5000
-
 GROUP BY base_category, category
-
 HAVING total > 250000
-
 ORDER BY total DESC;
-
-
 
 #找出购买产品和未购买产品的会员年龄和性别分布#
-
 SELECT m.member_id, age, gender,
-
 CASE
-
 WHEN s.member_id IS NOT NULL THEN 'Y' ELSE 'N'
-
 END AS flag
-
 FROM sales s
-
 RIGHT JOIN members m
-
 ON m.member_id=s.member_id;
 
-
-
 #找到不同年龄段的顾客平均消费金额的差异#
-
 SELECT AVG(amount),
-
 CASE
-
 WHEN age<=35 THEN '青年'
-
 WHEN age>=36 AND age<=50 THEN '中年'
-
 WHEN age>51 THEN '老年'
-
 END AS flag
-
 FROM sales s
-
 INNER JOIN members m
-
 ON s.member_id=m.member_id
-
 GROUP BY flag;
-
-
-
------------------------------------------------------
